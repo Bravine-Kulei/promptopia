8bb1e0a274e82b03ca54e6634c89ca73c71e59e1
bravin kipkulei <bravinkipkulei@gmail.com>
2024-05-13 16:38:39 +0300

    setting up basic-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <section className='w-full flex-center flex-col '>
+    <section className="w-full flex-center flex-col ">
+      <h1 className="head_text text-center">
+        Discover & share
+        <br className="max md:hidden" />
+        <span className="orange_gradient">AI-powered  Prompts</span>
+      </h1>
+      <p className="desc text-center ">
+        Create and share AI prompts to inspire your next project or to help
+        others get started.
+      </p>
 
+      {/*feed section  */}
     </section>
-  )
-}
+  );
+};
 
 export default Home;
